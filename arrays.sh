86fc21fc8bb12d1fc260b5e11fdd496d4ad9b366
ahopgood <alex.hopgood@gmail.com>
2017-07-30 16:20:14 +0100

    Example of using "declare" keyword to create an array.

---
+++ arrays.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+function log() {
+ echo "[$0] $1"
+}
+
+declare -a TEST_ARRAY
+
+TEST_ARRAY=(
+	"value1",
+	"value2"
+)
+
+echo "Print all contents: ${TEST_ARRAY[@]}"
+
+echo "Size of the array ${#TEST_ARRAY[*]}"
+
+echo "Iterable of the array ${!TEST_ARRAY[*]}"
+
+declare -A STRING_ARRAY
+
+STRING_ARRAY[myindex]="Hello there Mr Anderson"
+STRING_ARRAY[machineindex]="Agent Smith"
+
+echo "Print all contents: ${STRING_ARRAY[@]}"
+
+echo "Iterable of the arrray ${!STRING_ARRAY[*]}"
+
+echo "Accessing via myindex ${STRING_ARRAY[myindex]}"
